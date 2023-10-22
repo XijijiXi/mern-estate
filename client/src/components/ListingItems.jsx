b132e9f24cf7a615fed6400b4c0eb8b30bc30ef5
XijijiXi <y1006884462@gmail.com>
2023-10-21 21:46:50 -0700

    completed home page

--- client/src/components/ListingItems.jsx
+++ client/src/components/ListingItems.jsx
@@ -4,7 +4,7 @@ import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItems({ listing }) {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[250px]">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={listing.imageUrls[0]}
